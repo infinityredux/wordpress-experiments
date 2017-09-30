be0a282e6e46d052c8ba29d98c36684eaf884665
Nick <kedomir@gmail.com>
2017-09-30 17:00:51 +0100

    Updating documentation and removing stubs of old functions.

--- source/infinityredux-util/includes/InfinityReduxFrameWidget.php
+++ source/infinityredux-util/includes/InfinityReduxFrameWidget.php
@@ -1,13 +1,13 @@
 <?php
-// Register and load the widget
-function wpb_load_widget() {
 
-}
-// add_action( 'widgets_init', 'wpb_load_widget' );
-
-// Creating the widget
+/**
+ * Class InfinityReduxFrameWidget
+ */
 class InfinityReduxFrameWidget extends WP_Widget {
 
+	/**
+	 * Static function to be called during 'widgets_init' action.
+	 */
     static function init() {
 	    register_widget( 'InfinityReduxFrameWidget' );
     }
@@ -20,7 +20,11 @@ class InfinityReduxFrameWidget extends WP_Widget {
 		);
 	}
 
-	// Creating widget front-end
+	/**
+     * Creating widget front-end
+	 * @param array $args
+	 * @param array $instance
+	 */
 	public function widget( $args, $instance ) {
 		$title = apply_filters( 'widget_title', $instance['title'] );
 
@@ -32,9 +36,13 @@ class InfinityReduxFrameWidget extends WP_Widget {
 		// This is where you run the code and display the output
 		echo __( 'Hello, World!', 'infinityredux' );
 		echo $args['after_widget'];
-	}
+	}/** @noinspection PhpDocMissingReturnTagInspection */
 
-	// Widget Backend
+	/**
+     * Widget Backend
+	 * @param array $instance
+     *
+	 */
 	public function form( $instance ) {
 		if ( isset( $instance[ 'title' ] ) ) {
 			$title = $instance[ 'title' ];
@@ -49,12 +57,20 @@ class InfinityReduxFrameWidget extends WP_Widget {
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 		</p>
 		<?php
+
+        //do nothing
 	}
 
-	// Updating widget replacing old instances with new
+	/**
+     * Updating the widget replaces the old instance with new
+	 * @param array $new_instance
+	 * @param array $old_instance
+	 *
+	 * @return array
+	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 		return $instance;
 	}
-} // Class wpb_widget ends here
+}
